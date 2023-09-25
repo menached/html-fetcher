13475bdfca9df2e2a4c20ef4bd16fb8d71efbdcc
David Menache <dmenache@devopsandplatforms.com>
2023-09-25 11:07:46 -0600

    updates to use beautiful soup to filter

--- Discover/fetch_html.py
+++ Discover/fetch_html.py
@@ -1,4 +1,8 @@
 import requests
+import time
+import pprint
+from bs4 import BeautifulSoup
+
 
 # Fetch page 1
 url = "https://discoversjds.com/property-search/?keyword&location=any&status=any&type=any&bedrooms=any&bathrooms=any&min-price=any&max-price=any&min-area&max-area"
@@ -6,6 +10,26 @@ filename = "page1.html"
 response = requests.get(url)
 with open(f"fetched/Discover/{filename}", "wb") as file:
     file.write(response.content)
+    with open(f"fetched/Discover/{filename}", "r") as file:
+        soup = BeautifulSoup(file.read(), 'html.parser')
+        filtered_articles = soup.find_all('article', class_='property-item')
+
+        urls = []
+        for article in filtered_articles:
+            url = article.find('a')['href']
+            urls.append(url)
+
+        pprint.pprint(urls)
+        time.sleep(3)
+        # filtered_articles = soup.find_all('article', class_='property-item')
+
+        # for article in filtered_articles:
+            # print(article.prettify())  # or do something else with the filtered articles
+
+
+        #print(result)
+        #time.sleep(4)
+
 
 # Fetch pages 2 to 20
 for i in range(2, 21):
@@ -14,3 +38,14 @@ for i in range(2, 21):
     response = requests.get(url)
     with open(f"fetched/Discover/{filename}", "wb") as file:
         file.write(response.content)
+        with open(f"fetched/Discover/{filename}", "r") as file:
+            soup = BeautifulSoup(file.read(), 'html.parser')
+            filtered_articles = soup.find_all('article', class_='property-item')
+
+            urls = []
+            for article in filtered_articles:
+                url = article.find('a')['href']
+                urls.append(url)
+
+            pprint.pprint(urls)
+            time.sleep(3)
